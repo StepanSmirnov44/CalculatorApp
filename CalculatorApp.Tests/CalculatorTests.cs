eb40803bc50f3d3171eed89b5dd8c3e95e4658f9
Stepan <VadimkaSM44@gmail.com>
2026-05-21 01:05:31 +0300

    Добавьте файлы проекта.

---
+++ CalculatorApp.Tests/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using CalculatorApp;
+
+namespace CalculatorApp.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_2and3_Returns5()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            int result = calc.Add(2, 3);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+    }
+}
